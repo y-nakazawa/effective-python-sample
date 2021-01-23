4c52fd60cd43c041546e69e1a610d07966b07075
中澤祐一 <y.nakazawa1220@gmail.com>
2021-01-23 21:15:00 +0900

    項目39のサンプル作成

---
+++ section39/section39.py
@@ -0,0 +1,73 @@
+
+import os, random
+from threading import Thread
+
+def write_test_files(tmpdir):
+    os.makedirs(tmpdir)
+    for i in range(100):
+        with open(os.path.join(tmpdir, str(i)),'w') as f:
+            f.write('\n' * random.randint(0, 100))
+
+def generate_input(data_dir):
+    for name in os.listdir(data_dir):
+        yield PathInputData(os.path.join(data_dir, name))
+
+def create_workers(input_list):
+    workers = []
+    for input_data in input_list:
+        workers.append(LineCountWorker(input_data))
+    return workers
+
+def execute(workers):
+    threads = [Thread(target=w.map) for w in workers]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+
+    first, *rest = workers
+    for worker in rest:
+        first.reduce(worker)
+    return first.result
+
+def mapreduce(data_dir):
+    inputs = generate_input(data_dir)
+    workers = create_workers(inputs)
+    return execute(workers)
+
+class InputData:
+    def read(self):
+        raise NotImplementedError
+
+class PathInputData(InputData):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+
+    def read(self):
+        with open(self.path) as f:
+            return f.read()
+
+class Worker:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.result = None
+
+    def map(self):
+        raise NotImplementedError
+
+    def reduce(self, other):
+        raise NotImplementedError
+
+class LineCountWorker(Worker):
+    def map(self):
+        data = self.input_data.read()
+        self.result = data.count('\n')
+
+    def reduce(self, other):
+        self.result += other.result
+
+if __name__ == "__main__":
+    tmpdir = 'test_inputs'
+    write_test_files(tmpdir)
+
+    result = mapreduce(tmpdir)
+    print(f'There are {result} lines')
